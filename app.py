a4361416b42584ef1a22d7c9322b0e0bd260b44b
Majed <majed@mbpalkhasbmajed.lan>
2020-06-22 14:23:38 +0300

    forth

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # app.py
 from flask import Flask, request, jsonify
 import requests
+from bs4 import BeautifulSoup
+import html5lib
+
 app = Flask(__name__)
+
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
@@ -26,6 +30,7 @@ def respond():
     # Return the response in json format
     return jsonify(response)
 
+
 @app.route('/post/', methods=['POST'])
 def post_something():
     param = request.form.get('name')
@@ -35,7 +40,7 @@ def post_something():
         return jsonify({
             "Message": f"Welcome {name} to our awesome platform!!",
             # Add this option to distinct the POST request
-            "METHOD" : "POST"
+            "METHOD": "POST"
         })
     else:
         return jsonify({
@@ -43,10 +48,26 @@ def post_something():
         })
 
 # A welcome message to test our server
+data = {
+  'loginForm:userType': '1',
+  'loginForm:username': '439101835',
+  'loginForm:password': '1113583775',
+  'loginForm': 'loginForm',
+  'loginForm:_idcl': 'loginForm:loginUsersLink'
+}
+url = 'https://edugate.ksu.edu.sa/ksu/init'
+
 @app.route('/')
 def index():
-    r = requests.get('https://edugate.ksu.edu.sa/ksu/init')
-    return r.text
+    s = requests.session()
+    r = s.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    data['com.sun.faces.VIEW'] = soup.find('input', attrs={'name': 'com.sun.faces.VIEW'})['value']
+
+    r = s.post(url, data=data)
+    page = s.get('https://edugate.ksu.edu.sa/ksu/ui/student/student_transcript/index/studentTranscriptAllIndex.faces')
+  
+    return page.text
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
