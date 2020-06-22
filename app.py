8b190f2e82124073fc449394d3f8fe98a05ffd72
Majed <majed@mbpalkhasbmajed.lan>
2020-06-22 14:10:34 +0300

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 from flask import Flask, request, jsonify
+import requests
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
@@ -44,7 +45,8 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    r = requests.get('https://edugate.ksu.edu.sa/ksu/init')
+    return r.text
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
